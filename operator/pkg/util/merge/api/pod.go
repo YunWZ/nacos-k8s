d48a5f12800d4c15c49d3bd240b359795940f274
yunweizhan <yunweizhan@gmail.com>
2025-05-29 22:59:52 +0800

    add artifacthub-repo.yml

--- operator/pkg/util/merge/api/pod.go
+++
@@ -1,8 +0,0 @@
-package api
-
-// StatefulSetConfiguration holds the optional custom StatefulSet
-// that should be merged into the operator created one.
-type PodConfiguration struct {
-	// +kubebuilder:pruning:PreserveUnknownFields
-	SpecWrapper DeploymentSpecWrapper `json:"spec"`
-}
